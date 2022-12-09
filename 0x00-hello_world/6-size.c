cea1001cc9c117e87d7a2234510a5a1af3f67760
Cleve <clevemomanyi@gmail.com>
2022-12-09 01:45:47 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,7 @@ int main(void)
 	printf("Size of a char is: %d byte(s)\n", sizeof(char));
 	printf("Size of an int is: %d byte(s)\n", sizeof(int));
 	printf("Size of a long integer is: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long integer: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float is: %d byte(s)\n", sizeof(float));
 	return (0);
 }
