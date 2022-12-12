82aa7f89cbe7f9a851b578450579bbd0d86e4080
Cleve <clevemomanyi@gmail.com>
2022-12-12 08:58:27 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all base 10 numbers starting from 0
+ * Return: Always 0 when succesfull
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
