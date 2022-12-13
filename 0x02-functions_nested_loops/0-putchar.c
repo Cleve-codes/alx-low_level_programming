200f994f251d6f82c1f69fe73dbade7dba5cd4f9
Cleve <clevemomanyi@gmail.com>
2022-12-13 10:38:00 -0800

    #include <unistd.h>
    /**
    
     * _putchar - writes the character c to stdout
    
     * @c: The character to print
    
     *
    
     * Return: On success 1.
    
     * On error, -1 is returned, and errno is set appropriately.
    
     */
    
    int _putchar(char c)
    
    {
    
    return (write(1, &c, 1));
    
    /

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Prints the word _putchae then a new line
+ * Return: Always returns 0 when succesfull
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
