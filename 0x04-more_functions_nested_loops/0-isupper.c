b8e73bd0ff917cd0048b8b83e170a171bb9c2179
Cleve <clevemomanyi@gmail.com>
2022-12-15 10:31:21 -0800

    Upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - Checks for uppercase character
+ * @c: Character place holder
+ * Return: 1 if ch is uppercase 0 if else
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
