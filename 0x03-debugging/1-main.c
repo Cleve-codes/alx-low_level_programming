fd0f3778bd068742509b0fae7dd4cc452559e5fb
Cleve <clevemomanyi@gmail.com>
2022-12-19 12:03:01 -0800

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 /**
- * main - causes an infinite loop
- *Return: 0
+ * main - an infinite loop in main
+ * Return: 0
  */
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
- 
+
 	i = 0;
-	
-	/*while (i < 10)
-	{
 
-		putchar(i);
+	/*while (I < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
 
-	}*/
+	printf("infinite loop avoided! \\o/\n");
 
-	printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
-
+	return (0);
 }
