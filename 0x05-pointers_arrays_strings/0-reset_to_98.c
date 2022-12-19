e275a01553dad3e5b7cee39fbcd8fcdf785ff16f
Cleve <clevemomanyi@gmail.com>
2022-12-19 05:50:59 -0800

    Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Resets an int value using a pointer to 98
+ * @n: integer place holder
+ *
+ * Return: Nothing to return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
