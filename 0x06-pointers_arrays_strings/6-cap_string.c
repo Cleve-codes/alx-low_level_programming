dd116b42b47e0293c0ec12eb4287d6beb7eb3288
Cleve <clevemomanyi@gmail.com>
2022-12-21 22:22:36 -0800

    CPY

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * cap_string - Capitalizes all words of a string
- * @s: pointer to string
+ * cap_string - A function that capitalizes all words of a string
+ * @s: pointer to char
  * Return: @s
  */
 char *cap_string(char *s)
@@ -10,17 +10,17 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 97 && s[i] <= 12)
+		if (s[i] >= 97 && s[i] <= 122)
 		{
 			if (i == 0)
 			{
 				s[i] -= 32;
 			}
 			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
-				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
-				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
-				s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
-				s[i - 1] == 124)
+					s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+					s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+					s[i - 1] == 40 || s[i - 1] == 41 || s[i -1] ==123 ||
+					s[i - 1] == 124)
 			{
 				s[i] -= 32;
 			}
