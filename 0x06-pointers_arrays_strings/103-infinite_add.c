c8a67a322731e9c7aaa0fe28defb29bdca0e63a4
Cleve <clevemomanyi@gmail.com>
2022-12-22 09:30:37 -0800

    tricky

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * infinite_add - Adds 2 numbers
+ * @n1: First number
+ * @n2: Second number
+ * @r: Buffer that the function to store result
+ * @size_r: buffer size
+ * Return: Pointer to destination
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int 
