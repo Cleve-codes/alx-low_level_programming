16b3c212ee352026f5421785bb888de0b651bf72
Cleve <clevemomanyi@gmail.com>
2023-01-05 02:38:07 -0800

    String lenght

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * _strlen_recursion - Function to print string lenght
+ * @s: string pointer
+ * Return: Nothing
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
